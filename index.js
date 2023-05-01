b8a958994398e3f5a3117787c7cc735fa24ec1d2
unknown <akmalmaulfi@gmail.com>
2023-05-02 05:37:05 +0700

    add json, and add urlencoded middlewares

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express()
 const router = require("./routers")
 const PORT = process.env.PORT
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.use(router)
 
 app.listen(PORT, () => {
