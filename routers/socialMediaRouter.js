45812f373647d392a7041cbc1d0e3221c5f2b6d7
Nazrin- <jihadi013@gmail.com>
2023-05-06 13:45:46 +0800

    fixes some routes oopsie

--- routers/socialMediaRouter.js
+++ routers/socialMediaRouter.js
@@ -2,9 +2,9 @@ const router = require("express").Router()
 
 const socialMediaController = require('../controllers/socialMediaController')
 
-router.post('/socialmedias',socialMediaController.createsocialmedia)
-router.get('/socialmedias',socialMediaController.readsocialmedia)
-router.put('/socialmedias',socialMediaController.updatesocialmedia)
-router.delete('/socialmedias/:socialMediaId',socialMediaController.deletesocialmedia)
+router.post('/',socialMediaController.createsocialmedia)
+router.get('/',socialMediaController.readsocialmedia)
+router.put('/',socialMediaController.updatesocialmedia)
+router.delete('/:socialMediaId',socialMediaController.deletesocialmedia)
 
 module.exports = router
