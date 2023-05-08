5d6b36f0a33dad280c8c63d241298db8ccb8c491
unknown <akmalmaulfi@gmail.com>
2023-05-08 13:39:40 +0700

    update production config

--- config/config.js
+++ config/config.js
@@ -1,33 +1,42 @@
 require("dotenv").config()
+const Sequelize = require("sequelize")
 
-module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT,
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//     port: process.env.DB_PORT,
+//   },
+//   test: {
+//     username: "root",
+//     password: null,
+//     database: "database_test",
+//     host: "127.0.0.1",
+//     dialect: "mysql",
+//   },
+//   production: {
+//     username: process.env.DB_USERNAME.PRODUCTION,
+//     password: process.env.DB_PASSWORD_PRODUCTION,
+//     database: process.env.DB_DATABASE_PRODUCTION,
+//     host: process.env.DB_HOST_PRODUCTION,
+//     dialect: process.env.DB_DIALECT_PRODUCTION,
+//     port: process.env.DB_PORT_PRODUCTION,
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//   },
+// }
+
+const db = new Sequelize(process.env.DB_URI, {
+  define: {
+    timestamps: false,
   },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
-    username: "postgres",
-    password: "MR7LFIoThotUHutvvC7D",
-    database: "railway",
-    host: "containers-us-west-205.railway.app",
-    dialect: "postgres",
-    port: "5704",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  },
-}
+})
+
+module.exports = db
